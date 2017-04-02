9ef77c8bbac91ebfeb7039331cc69e5229b2af06
yoowonyoung <wonyoung0105@gmail.com>
2017-04-03 02:42:31 +0900

    Revert "Redmine 업로드"
    This reverts commit 6cfa0990c20c9a73138d99a0f21837616c4a6175.

--- doc/case-tool/redmine/redmine/docker-custompoint.sh
+++
@@ -1,8 +0,0 @@
-#ckeditor
-git clone https://github.com/a-ono/redmine_ckeditor.git /usr/src/redmine/plugins/redmine_ckeditor
-# Agile
-wget -O redmine_agile.zip http://email.redmineup.com/c/eJxFjkEOgjAQRU9TlmY6paVddGFivAZpZwZFoRhAub51ZfJXb_Hf4zjY4E1uxmh9JgB0OXjyPbIOzkEQsKB5UC2swvNY5P06zWmcmnukAOJ0GJIl471rGYnAWuhazixWmine9_2lzFnhte44jtP_hJa5smkkKZv0cyrpJmslBlGjMtd9eUpR5gJGgpMsCVAGz-igk0xdpTWNuoTOa92skctj4zeDBltjS_rI-nN8AaGcREo
-unzip redmine_agile.zip
-
-bundle install --without development test
-rake redmine:plugins:migrate RAILS_ENV=production
