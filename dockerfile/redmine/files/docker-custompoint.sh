be6ea463622fbdd983d61e6f52694ad9fff8ff4e
lkh4768 <lkh4768@outlook.kr>
2017-06-22 11:38:38 +0900

    Update docker-custompoint.sh

--- dockerfile/redmine/files/docker-custompoint.sh
+++ dockerfile/redmine/files/docker-custompoint.sh
@@ -33,8 +33,7 @@ if [ ! -d "$AGILE_DIR" ]
 then
     bundle lock --add-platform java
     bundle lock --add-platform x86-mingw32 x64-mingw32 x86-mswin32 java
-    wget -O redmine_agile.zip http://email.redmineup.com/c/eJxFzrFuwzAMBNCvkceAEiVSHjQUKPIbgSRSjVtbCRyn_v04U4Cb3nB3kloYI5ZhSiFKFHbADuXSyFnQygQBfA3Gw6qyTF2f99OSp3m4Ji6-2Tg2gCKFYkYmpeqLFQkkOA5zum7b3eCXcecj-76fPiX1thw2T1X7Qy9L7vlH10OQwVuD5-
-32p93gN6COpEUzOG1RHAFrqXyoQqicHUVrhzVJ_33IU8DC-2zP_7q-N15SxkPr
+    wget -O redmine_agile.zip http://email.redmineup.com/c/XXX
     unzip redmine_agile.zip -d /usr/src/redmine/plugins
 fi
 
